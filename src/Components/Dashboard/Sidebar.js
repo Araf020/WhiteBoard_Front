bc233dd09d6dcfe44804256b98dca7b2dc067a27
Araf020 <Araf020@users.noreply.github.com>
2022-08-08 13:52:44 +0600

    sidebar modifed

--- src/Components/Dashboard/Sidebar.js
+++ src/Components/Dashboard/Sidebar.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import {Link} from 'react-router-dom';
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
@@ -18,6 +19,18 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+
+// notice
+import NoticeICon from '@mui/icons-material/CampaignRounded';
+// courses
+import CourseIcon from '@mui/icons-material/CategoryRounded';
+// assignment pendiing
+import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
+// assignment completed
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
+// course materials
+import CourseMaterialIcon from '@mui/icons-material/FolderSpecialRounded';
+
 import { Grid } from '@mui/material';
 
 
@@ -133,7 +146,62 @@ export default function Sidebar() {
         </DrawerHeader>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key= "Course" disablePadding sx={{display: 'block'}}>
+            <ListItemButton sx={{
+              minHeight: 48,
+              justifyContent: open ? 'initial' : 'center',
+              px: 2.5,
+            }}>
+              <ListItemIcon 
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <CourseIcon />
+              </ListItemIcon>
+              <ListItemText primary="Courses" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key= "Assignments_u" disablePadding sx={{display: 'block'}}>
+            <ListItemButton sx={{
+              minHeight: 48,
+              justifyContent: open ? 'initial' : 'center',
+              px: 2.5,
+            }}>
+              <ListItemIcon 
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <AssignmentRoundedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Due Assignments" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key= "assignments_f" disablePadding sx={{display: 'block'}}>
+            <ListItemButton sx={{
+              minHeight: 48,
+              justifyContent: open ? 'initial' : 'center',
+              px: 2.5,
+            }}>
+              <ListItemIcon 
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <AssignmentTurnedInIcon />
+              </ListItemIcon>
+              <ListItemText primary="Completed Assignments" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
             <ListItem key={text} disablePadding sx={{ display: 'block' }}>
               <ListItemButton
                 sx={{
@@ -154,32 +222,48 @@ export default function Sidebar() {
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
             </ListItem>
-          ))}
+          ))} */}
         </List>
         <Divider />
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
+        <ListItem key= "files" disablePadding sx={{display: 'block'}}>
+            <ListItemButton sx={{
+              minHeight: 48,
+              justifyContent: open ? 'initial' : 'center',
+              px: 2.5,
+            }}>
+              <ListItemIcon 
                 sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
                 }}
               >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+                <CourseMaterialIcon />
+              </ListItemIcon>
+              <ListItemText primary="Course Materials" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key= "notice" disablePadding sx={{display: 'block'}}>
+            <ListItemButton sx={{
+              minHeight: 48,
+              justifyContent: open ? 'initial' : 'center',
+              px: 2.5,
+            }}>
+              <ListItemIcon 
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              > 
+                <NoticeICon  
+                
+                />
+              </ListItemIcon>
+              <ListItemText primary="Anouncements" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
         </List>
       </Drawer>
      
